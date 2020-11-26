dc5c63bc41105be665acc44d7d8683c948578297
Andrea-Oliveri <andrea.oliveri@epfl.ch>
2020-11-26 19:05:00 +0100

    Improved readibility of zoom function

--- Scripts/distortions.py
+++ Scripts/distortions.py
@@ -20,7 +20,9 @@ def normalize_bits(img, nb_bits=1):
     """
     Par default, normalise entre 0 et 1, mais si nb_bits>1 specified, it scales it to max representable by an unsigned of length nb_bits
     """
-    return (2**nb_bits - 1)*(img-np.min(img))/(np.max(img)-np.min(img))
+    min_val = np.min(img)
+    max_val = np.max(img)
+    return (2**nb_bits-1)*(img-min_val)/(max_val-min_val)
 
 
 def add_gaussian(img, amplitude, sigmaX=None, sigmaY=None, mu_x=None, mu_y=None, nb_bits=8):
@@ -42,6 +44,12 @@ def add_gaussian(img, amplitude, sigmaX=None, sigmaY=None, mu_x=None, mu_y=None,
     return normalize_bits(new_img, nb_bits)
 
 
+def add_gaussian_noise(img, mean, std, nb_bits=8):
+    gaussian = np.random.normal(mean, std, img.shape)
+    output = normalize_bits(img + gaussian, nb_bits)
+    return output
+
+
 def zoom_image(img, zoom_factor, val_padding=None):
     """
 
@@ -49,54 +57,37 @@ def zoom_image(img, zoom_factor, val_padding=None):
     if zoom_factor <= 0:
         raise ValueError("The zoom factor must be strictly positive")
     
-    height, width, _ = img.shape
-    width_zoom = int(width * zoom_factor)
-    height_zoom = int(height * zoom_factor)
-    
-    diff_width = width_zoom - width
-    diff_height = height_zoom - height
-    
-    if zoom_factor == 1:
+    elif zoom_factor == 1:
         return img
     
     elif zoom_factor > 1:
-        # Crop image
-        x1 = diff_width // 2
-        y1 = diff_height // 2
-        
-        x2 = x1 + width
-        y2 = y1 + height
-        
-        edges = np.array([x1,y1,x2,y2])
-        edges = edges / zoom_factor
-        x1, y1, x2, y2 = edges.astype(np.int)
-        cropped_img = img[y1:y2, x1:x2]
-        
         # Resize
-        output = cv2.resize(cropped_img, (width, height), interpolation=cv2.INTER_CUBIC)
+        new_img = cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor, interpolation=cv2.INTER_CUBIC)
+        
+        # Crop desired central portion
+        height, width, _      = img.shape
+        new_height, new_width = new_img.shape
+        line_start = (new_height - height) // 2
+        col_start  = (new_width  - width ) // 2
+        line_end   = line_start + height
+        col_end    = col_start  + width
+        output = new_img[line_start:line_end, col_start:col_end]
         
     elif zoom_factor < 1:
         # Resize
-        output = cv2.resize(img, (width_zoom, height_zoom), interpolation=cv2.INTER_CUBIC)
+        new_img = cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor, interpolation=cv2.INTER_CUBIC)
         
         if val_padding == None:
             val_padding = img.min()
         
         # Padding with val_padding
-        x1 = -diff_width // 2
-        y1 = -diff_height // 2
+        height, width, _      = img.shape
+        new_height, new_width = new_img.shape
+        pad_up    = (height - new_height) // 2
+        pad_down  = height - pad_up - new_height
+        pad_left  = (width - new_width) // 2
+        pad_right = width - pad_left - new_width
         
-        x2 = -diff_width - x1
-        y2 = -diff_height - y1
-        
-        pad_width = [(y1, y2), (x1, x2)]
-        
-        output = np.pad(output, pad_width, 'constant', constant_values = val_padding)
+        output = np.pad(new_img, ((pad_up, pad_down), (pad_left, pad_right)), 'constant', constant_values = val_padding)
 
     return output.reshape(img.shape)
-
-
-def add_gaussian_noise(img, mean, std, nb_bits=8):
-    gaussian = np.random.normal(mean, std, img.shape)
-    output = normalize_bits(img + gaussian, nb_bits)
-    return output
