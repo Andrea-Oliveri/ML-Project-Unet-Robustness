4a83cbd00b97e7573e8338ed80f8b118e0614d74
BassLC <bernardolimaconde@gmail.com>
2020-11-28 17:09:20 +0000

    Add Noise remover

---
+++ Scripts/noise_removal.py
@@ -0,0 +1,11 @@
+import cv2
+
+def de_noise(noisy_img):
+    #-CV2 crazy denoiser magic-
+    # return cv2.fastNlMeansDenoising(noisy_img, h=10)
+    #-Gaussian Blur-
+    sigmaX = (noisy_img.shape[0])/80
+    sigmaY = (noisy_img.shape[1])/80
+    return cv2.GaussianBlur(noisy_img, ksize=(0,0), sigmaX=sigmaX, sigmaY=sigmaY)
+    #-Median Blur-
+    #return cv2.medianBlur(noisy_img, 7)
